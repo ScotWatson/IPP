a38302c48b88089a91feb46b3b5f8901df27bb5d
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-06-05 13:21:29 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -9,15 +9,30 @@ window.addEventListener("load", function () {
   document.body.appendChild(document.createTextNode("IP Address of printer: "));
   const elemIPAddress = document.createElement("input");
   document.body.appendChild(elemIPAddress);
-  const btnFetch = document.createElement("button");
-  btnFetch.innerHTML = "Fetch";
-  document.body.appendChild(btnFetch);
-  btnFetch.addEventListener("click", function (evt) {
+  const btnFetch1 = document.createElement("button");
+  btnFetch1.innerHTML = "Fetch 1";
+  document.body.appendChild(btnFetch1);
+  btnFetch1.addEventListener("click", function (evt) {
     const dest1 = "ipps://" + elemIPAddress.value;
-    const dest2 = "https://" + elemIPAddress.value + ":443";
     const data = Uint8Array.from([ 0x01, 0x00, 0x00, 0x0A, 0x03, 0x00]);
     const myBlob = new Blob(data, { type: "application/ipp" });
     fetch(dest1, { method: "POST", body: myBlob}).then(showResponse, showError);
+    function showResponse(response) {
+      console.log(response);
+      alert("done");
+    }
+    function showError(error) {
+      console.error(error);
+      alert("done");
+    }
+  });
+  const btnFetch2 = document.createElement("button");
+  btnFetch2.innerHTML = "Fetch 2";
+  document.body.appendChild(btnFetch2);
+  btnFetch2.addEventListener("click", function (evt) {
+    const dest2 = "https://" + elemIPAddress.value + ":631";
+    const data = Uint8Array.from([ 0x01, 0x00, 0x00, 0x0A, 0x03, 0x00]);
+    const myBlob = new Blob(data, { type: "application/ipp" });
     fetch(dest2, { method: "POST", body: myBlob}).then(showResponse, showError);
     function showResponse(response) {
       console.log(response);
