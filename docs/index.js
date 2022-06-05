ec3d94ea194f6b7d674375666d07b2d23b77e881
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-06-05 12:15:15 -0400

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,28 @@
+/*
+(c) 2022 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+window.addEventListener("load", function () {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+  document.body.appendChild(document.createTextNode("IP Address of printer: "));
+  const elemIPAddress = document.createElement("input");
+  document.body.appendChild(elemIPAddress);
+  const btnFetch = document.createElement("button");
+  document.body.appendChild(btnFetch);
+  btnFetch.addEventListener("click", function (evt) {
+    const dest = "ipp://" + elemIPAddress.value;
+    const data = Uint8Array.from([ 0x01, 0x00, 0x00, 0x0A, 0x03, 0x00]);
+    const myBlob = new Blob(data, { type: "application/ipp" });
+    fetch(dest, { method: "POST", body: myBlob}).then(showResponse, showError);
+    function showResponse(response) {
+      console.log(response);
+      alert("done");
+    }
+    function showError(error) {
+      console.error(error);
+      alert("done");
+    }
+  });
+});
